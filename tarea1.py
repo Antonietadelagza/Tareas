9a935daaef77c379f20c803b27547794cb820fce
Antonietadelagza <80795252+Antonietadelagza@users.noreply.github.com>
2021-03-23 14:26:16 -0600

    Add files via upload

---
+++ tarea1.py
@@ -0,0 +1,11 @@
+# ANTONIETA DE LA GARZA
+print('\n\tPROGRAMA QUE CALCULA EL PROMEDIO CON 5 CALIFICACIONES')
+print('\n\t\tINGRESAR DATOS')
+nombre= str(input('\n\t\tNombre:'))
+cal1= float(input('\n\t\tCalificación 1: '))
+cal2= float(input('\n\t\tCalificación 2: '))
+cal3= float(input('\n\t\tCalificación 3: '))
+cal4= float(input('\n\t\tCalificación 4: '))
+cal5= float(input('\n\t\tCalificación 5: '))
+promedio=(cal1+cal2+cal3+cal4+cal5)/5
+print ('\n\t\tEl promedio de: ',nombre,' es: ',promedio )
