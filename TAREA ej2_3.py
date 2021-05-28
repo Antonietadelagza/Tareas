46c74c4c007af4d180315052d090240ebd1cf7fa
Antonietadelagza <80795252+Antonietadelagza@users.noreply.github.com>
2021-05-28 14:48:02 -0500

    Add files via upload

--- TAREA ej2_3.py
+++ TAREA ej2_3.py
@@ -6,11 +6,11 @@ print('\ttiene un descuento del 60%. Programa que pregunte el número de barras 
 print('\tque no son del día. Después el programa debe mostrar el precio habitual de una barra')
 print('\tde pan, el descuento que se le hace por no ser fresca y el total a pagar\n')
 barras = int(input('\n\t¿Cuántas barras frías vendiste? '))
-precio = 27 
-des = barras*0.6*precio
-pago = barras * des
-print('\n\tEl precio de una barra de pan fresca es: $', precio)
-print('\n\tEl total por ',barras, 'barras es: $',precio*barras)
-print('\tEl  60% descuento por barra fría es: -',round(des,2))
+precio = 10.8 
+#des = 0.6*prec
+pago = precio*barras
+print('\n\tEl precio de una barra de pan fresca es: $27')
+print('\n\tEl total por ',barras, 'barras es: $', 27*barras)
+print('\tEl  60% descuento por barra fría es: -16.2')
 print('\tEl total a pagar por ',barras, 'frías es: $',round(pago,2))
 print('\n\n')
